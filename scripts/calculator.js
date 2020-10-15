7f6764b031fee33a8974e1e9b8035e19c71210e5
Funby <jamiuaminat@yahoo.com>
2020-10-16 00:48:27 +0100

    yyy

---
+++ scripts/calculator.js
@@ -0,0 +1,27 @@
+(function(){
+let buttons = document.querySelectorAll('.btn');
+let screen = document.querySelector('.screen');
+let equal = document.querySelector('.btn-equal');
+let clear = document.querySelector('.btn-clear');
+
+buttons.forEach(function(button){
+  button.addEventListener('click', function(e){
+    let value = e.target.dataset.num;
+    screen.value += value;
+  })
+});
+
+equal.addEventListener('click', function(e){
+  if(screen.value === ''){
+    screen.value = 'Please Enter a Value';
+  } else {
+    let answer = eval(screen.value);
+    screen.value = answer;
+  }
+})
+
+clear.addEventListener('click', function(e){
+  screen.value = '';
+})
+
+})();
